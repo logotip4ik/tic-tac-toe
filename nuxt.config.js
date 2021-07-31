520a1b792edfcf2a8e9b80733ba839100e5e3dce
logotip4ik <bogdankostyuk12@gmail.com>
2021-07-31 15:48:02 +0300

    update: added firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,26 +3,22 @@ export default {
   head: {
     title: 'fire-tic-tac-toe',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -31,13 +27,24 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    'nuxt-vite',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ['@nuxtjs/firebase'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+  firebase: {
+    // options
+    config: {
+      apiKey: 'AIzaSyDtWw1nR426v6p3M-BMCKwizqimI7tnPmI',
+      authDomain: 'tic-tac-toe-8037e.firebaseapp.com',
+      projectId: 'tic-tac-toe-8037e',
+      storageBucket: 'tic-tac-toe-8037e.appspot.com',
+      messagingSenderId: '1041126153009',
+      appId: '1:1041126153009:web:408ad7d0b28b18d701ee10',
+    },
+    services: { firestore: true, auth: true },
+  },
 }
